01207963ddcbf4a30458466aca21b42ceecd02d3
Alazar Gebremedhin <alazar4018@gmail.com>
2025-02-10 09:38:52 +0300

    - Add vendor/ directory.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ucms/ucms',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd19caa08919179856384db14b4d12689cb6a20e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ucms/ucms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd19caa08919179856384db14b4d12689cb6a20e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
