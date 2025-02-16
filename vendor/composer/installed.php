1c6ce8756442246e15feab1bc866196c2ce6959c
Alazar Gebremedhin <alazar4018@gmail.com>
2025-02-16 09:31:54 +0300

    Add composer vendor.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ucms/ucms',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1b733b444f8925c74d2749d072e95137668aca2d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ucms/ucms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1b733b444f8925c74d2749d072e95137668aca2d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
